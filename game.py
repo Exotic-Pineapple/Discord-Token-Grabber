987db894a2f9bdd4347b2f06ba68ccc67703c3d7
Tharakesh-coder <69941591+Tharakesh-coder@users.noreply.github.com>
2022-11-23 13:42:52 -0600

    i finally found some snacks.
    but now im in the basement locked up making yt vid for the blue lobster.

--- game.py
+++ game.py
@@ -17,7 +17,7 @@ def main():
     
     headers = {
         'Content-Type': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
+        'User-Agent': ' random crap. idk. it needs this to work. so... '
     }
 
     payload = json.dumps({'content': message})
